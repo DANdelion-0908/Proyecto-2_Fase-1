cd638373bc5d9718d33097a803ae8bb197bdeeac
Dan <98132259+Dan090807@users.noreply.github.com>
2023-05-07 18:39:26 -0600

    Added Burger Class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+/*--------------------------------------------------
+ * UNIVERSIDAD DEL VALLE DE GUATEMALA
+ * ALGORITMOS Y ESTRUCTURAS DE DATOS
+ * 
+ * Proyecto 2
+ * Fase 1
+ * 
+ * Tema: Algoritmo de Recomendación de hamburguesas
+ * -------------------------------------------------
+ */
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        System.out.println("Algoritmo de Recomendación de Hamburguesas");
     }
 }
---
+++ src/Burger/Burger.java
@@ -0,0 +1,140 @@
+package Burger;
+
+import java.util.ArrayList;
+
+public class Burger {
+ 
+    private String bread;
+    private String  meat; 
+    private String tomato;
+    private String lettuce;
+    private String onion;
+
+    private boolean sauce;
+    private String ketchup;
+    private String mayonnaise;
+    private String mustard;
+    private String spicy;
+    private String bbq;
+
+    private boolean extras;
+    private String egg;
+    private String beacon;
+
+    private ArrayList<String> burgerUnit = new ArrayList<>();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public String getBread() {
+        return bread;
+    }
+    public void setBread(String bread) {
+        this.bread = bread;
+    }
+    public String getMeat() {
+        return meat;
+    }
+    public void setMeat(String meat) {
+        this.meat = meat;
+    }
+    public String getTomato() {
+        return tomato;
+    }
+    public void setTomato(String tomato) {
+        this.tomato = tomato;
+    }
+    public String getLettuce() {
+        return lettuce;
+    }
+    public void setLettuce(String lettuce) {
+        this.lettuce = lettuce;
+    }
+    public String getOnion() {
+        return onion;
+    }
+    public void setOnion(String onion) {
+        this.onion = onion;
+    }
+    public boolean isSauce() {
+        return sauce;
+    }
+    public void setSauce(boolean sauce) {
+        this.sauce = sauce;
+    }
+    public String getKetchup() {
+        return ketchup;
+    }
+    public void setKetchup(String ketchup) {
+        this.ketchup = ketchup;
+    }
+    public String getMayonnaise() {
+        return mayonnaise;
+    }
+    public void setMayonnaise(String mayonnaise) {
+        this.mayonnaise = mayonnaise;
+    }
+    public String getMustard() {
+        return mustard;
+    }
+    public void setMustard(String mustard) {
+        this.mustard = mustard;
+    }
+    public String getSpicy() {
+        return spicy;
+    }
+    public void setSpicy(String spicy) {
+        this.spicy = spicy;
+    }
+    public String getBbq() {
+        return bbq;
+    }
+    public void setBbq(String bbq) {
+        this.bbq = bbq;
+    }
+    public boolean isExtras() {
+        return extras;
+    }
+    public void setExtras(boolean extras) {
+        this.extras = extras;
+    }
+    public String getEgg() {
+        return egg;
+    }
+    public void setEgg(String egg) {
+        this.egg = egg;
+    }
+    public String getBeacon() {
+        return beacon;
+    }
+    public void setBeacon(String beacon) {
+        this.beacon = beacon;
+    }
+}
